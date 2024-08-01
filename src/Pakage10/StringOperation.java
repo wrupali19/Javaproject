d5930bdeff310fbeed340bf1d2f409e2c9e512ed
wrupali19 <165141202+wrupali19@users.noreply.github.com>
2024-08-01 16:40:11 +0530

    Update StringOperation.java

--- src/Pakage10/StringOperation.java
+++ src/Pakage10/StringOperation.java
@@ -4,7 +4,7 @@ public class StringOperation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.out.println("String opretion");
+            System.out.println("String opretion in QA");
 	}
 
 }
